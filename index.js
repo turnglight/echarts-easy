6d6b7aa67d70a3b10b529946da68fae07091d30b
turnglight <335771269@qq.com>
2020-06-11 18:12:37 +0800

    提交代码

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 /**
- *based on echarts4.8.0
- *author:frontlich
- */
+ * based on echarts4.8.0
+ * author: dyn
+ **/
 (function(window, ec, undefined){
-
     var _charts = window.charts;
-
     var option = {};
-
     //Object.assign()   ES2015
     //该方法用于将所有可枚举的属性的值从一个或多个源对象复制到目标对象。它将返回目标对象。
     if (typeof Object.assign != 'function') {
@@ -16,7 +13,6 @@
             if (target == null) {
                 throw new TypeError('Cannot convert undefined or null to object');
             }
-
             target = Object(target);
             for (var index = 1; index < arguments.length; index++) {
                 var source = arguments[index];
@@ -30,97 +26,98 @@
             }
             return target;
         };
-    };
+    }
 
     function clone(target){          //克隆对象
-
         if(typeof target != 'object' || target == null){
             return target
-        };
-
+        }
         if(target instanceof Array){
             var newArr = [];
             for(var i = 0; i < target.length; i++){
                 newArr.push(clone(target[i]));
             }
             return newArr;
-        };
-
+        }
         var newObj = new Object();
-
         for(var i in target){
             newObj[i] = clone(target[i]);
         }
-
         return newObj;
-    };
+    }
 
     var Charts = function(type, selector){
-
         this.option = clone(option[type] || {});
-
         this.type = type;
-
         this.chart = ec.init(document.querySelector(selector));
-
+        this.queue = [];
     };
 
     Charts.prototype = {
-
-        title : function(text, config){         //设置标题
-
-            var oTitle = this.option.title || {};
-
-            this.option.title = config ? Object.assign(oTitle, config) : oTitle;
-
+        setTitle : function(text){         //设置标题
             this.option.title.text = text;
-
-            return this;
-        },
-        gird : function(show) {
-            this.option[gird].show = show;
             return this;
         },
         legend : function(data, config){     //设置图例
-
             var oLegend = this.option.legend || {};
-
             this.option.legend = config ? Object.assign(oLegend, config) : oLegend;
-
             this.option.legend.data = data;
-
+            return this;
+        },
+        setXAxisSplitLine : function(show){
+            this.option['xAxis'].splitLine.show = show;
+            return this;
+        },
+        setYAxisSplitLine : function(show){
+            this.option['yAxis'].splitLine.show = show;
             return this;
         },
         // 设置坐标轴的显示间隔
-        axisInterval: function(axis, interval='auto'){
-            this.option[axis].axisLabel.interval = interval;
+        setXAxisInterval: function(interval='auto'){
+            this.option['xAxis'].axisLabel.interval = interval;
             return this;
         },
-        // 设置坐标轴的字体样式
-        axisStyle: function(axis, fontStyle='normal', fontWeight='normal', color='rgb(0,0,0)'){    //设置x,y数轴的字体风格
-            this.option[axis].axisLabel.fontStyle = fontStyle;
-            this.option[axis].axisLabel.fontWeight = fontWeight;
-            this.option[axis].axisLabel.color = color;
+        // 设置坐标轴的显示间隔
+        setYAxisInterval: function(interval='auto'){
+            this.option['yAxis'].axisLabel.interval = interval;
             return this;
         },
         // 设置坐标轴的字体
-        axisFontStyle: function(axis,fontStyle='normal'){
-            this.option[axis].axisLabel.fontStyle = fontStyle;
+        setXAxisFontStyle: function(fontStyle='normal'){
+            this.option['xAxis'].axisLabel.fontStyle = fontStyle;
+            return this;
+        },
+        // 设置坐标轴的字体
+        setYAxisFontStyle: function(fontStyle='normal'){
+            this.option['yAxis'].axisLabel.fontStyle = fontStyle;
             return this;
         },
         // 设置坐标轴的字体大小
-        axisFontWeight: function(axis,fontWeight='normal'){
-            this.option[axis].axisLabel.fontWeight = fontWeight;
+        setXAxisFontWeight: function(fontWeight='normal'){
+            this.option['xAxis'].axisLabel.fontWeight = fontWeight;
+            return this;
+        },
+        // 设置坐标轴的字体大小
+        setYAxisFontWeight: function(fontWeight='normal'){
+            this.option['yAxis'].axisLabel.fontWeight = fontWeight;
             return this;
         },
         // 设置坐标轴的字体颜色
-        axisFontColor: function(axis,color='rgb(0,0,0)'){
-            this.option[axis].axisLabel.color = color;
+        setXAxisFontColor: function(color='rgb(0,0,0)'){
+            this.option['xAxis'].axisLabel.color = color;
             return this;
         },
-        axis : function(axis, type, format,config){    //设置坐标轴
-            var oAxis = this.option[axis] || {};
-            this.option[axis] = config ? Object.assign(oAxis, config) : oAxis;
+        // 设置坐标轴的字体颜色
+        setYAxisFontColor: function(color='rgb(0,0,0)'){
+            this.option['yAxis'].axisLabel.color = color;
+            return this;
+        },
+        setXAxisMinAndMax: function(min = 0 , max){
+            this.option['xAxis'].min = min;
+            this.option['yAxis'].max = max;
+        },
+        // 设置坐标类型
+        setAxisType : function(axis, type, format){    //设置坐标轴
             if(typeof(type) == "undefined" || type == 'string') {
                 this.option[axis].type = 'category';
             }else if(type == 'time'){
@@ -147,37 +144,60 @@
                         return texts;
                     }
                 }
-
             }else{
                 this.option[axis].type = type;
             }
             return this;
         },
-
-        series : function(series, config){          //type:类型， series:同ehcarts的series
-
+        setSeries : function(series){          //type:类型， series:同ehcarts的series
             var oSeries = clone(this.option.series || [{}]);
-
-            this.option.series = clone(config ? [Object.assign(oSeries[0], config)] : oSeries);
-
+            this.option.series = oSeries;
             for(var i = 0; i < series.length; i++){
                 this.option.series[i] = Object.assign({type:this.type}, oSeries[0], series[i]);
-            };
-
+            }
             return this;
         },
-
+        setXAxisData : function(data){
+            var oData = clone(this.option['xAxis'].data || [{}]);
+            this.option['xAxis'].data = data;
+            return this;
+        },
+        setYAxisData : function(data){
+            var oSeries = new Array(data.length);
+            this.option.series = oSeries;
+            for(var i = 0; i < data.length; i++){
+                this.option.series[i] = Object.assign({type:this.type}, [{data:[]}]);
+                this.option.series[i].data = data[i];
+            }
+            return this;
+        },
+        setChartInterval: function(mills){
+            setInterval(function(){
+                // 动态刷新坐标点，类型心电图，push一个，pop一个
+                point = this.charts.queue.pop();
+                if(point){
+                    data = this.charts.option.series[0].data;
+                    let array = Object.assign([], data);
+                    array.splice(0, 0, point[0]);
+                    array.pop();
+                    this.charts.option.series[0].data = array;
+                    this.charts.chart.setOption(this.charts.option)
+                }
+            }, mills);
+            return this;
+        },
+        pushPoint: function(point){
+            this.queue.push(point);
+            return this;
+        },
         create : function(){              //创建图表
-            //console.log(this.option);
+            console.log(this.option);
             this.chart.setOption(this.option);
         }
     };
-
-
     var charts = function(type, selector){
         return new Charts(type, selector);
     };
-
     charts.setDefault = function(type, public){   //设置所有图表默认设置
         data = {
             title:{
@@ -189,13 +209,13 @@
             tooltip:{
                 formatter:'{c}'
             },
-            grid:{
-                show: false
-            },
             xAxis:{
+                type: 'category',
                 splitLine:{
-                    show:false
+                    show: false
                 },
+                min: 0,
+                max: 50,
                 axisLabel:{
                     color: 'rgb(0, 0, 0)',
                     interval: 'auto',
@@ -208,8 +228,9 @@
 
             },
             yAxis:{
+                type: 'value',
                 splitLine:{
-                    show:false
+                    show: false
                 },
                 axisLabel:{
                     color: 'rgb(0, 0, 0)',
@@ -217,13 +238,10 @@
                     fontWeight: 'normal'
                 },
             }
-        }
+        };
         option[type] = Object.assign(public || {}, data || {});
-
     };
-
     charts.changeDefault = function(type, newItem){       //只能添加和更改默认设置项，不能删除
-
         function change(option, newItem){
             for(var j in newItem){
                 if(option.hasOwnProperty(j)){
@@ -236,37 +254,28 @@
                     option[j] = newItem[j];
                 }
             }
-        };
-
+        }
         change(option[type], newItem);
     };
 
     charts.cloneDefault = function(type, source){
         option[type] = clone(option[source]);
     };
-
     charts.extend = function(name, fun){
-
         for(var key in Charts.prototype){
             if(name === key){
                 console.log('the key has been defined');
                 return false;
             }
-        };
-
+        }
         Charts.prototype[name] = fun;
     };
-
     charts.noConflict = function(){       //解决变量冲突
-
         if(window.charts === charts){
             window.charts = _charts;
-        };
-
+        }
         return charts;
     };
-
     window.charts = charts;
     //module.exports = charts;
-
 })(window, echarts);
--- readme.md
+++ readme.md
@@ -1,8 +1,115 @@
+<!-- START doctoc generated TOC please keep comment here to allow auto update -->
+<!-- DON'T EDIT THIS SECTION, INSTEAD RE-RUN doctoc TO UPDATE -->
+**Table of Contents**  *generated with [DocToc](https://github.com/thlorenz/doctoc)*
+
+- [api](#api)
+    - [setTitle](#settitle)
+    - [setXAxisFontStyle](#setxaxisfontstyle)
+    - [setYAxisFontStyle](#setyaxisfontstyle)
+    - [setXAxisFontWeight](#setxaxisfontweight)
+    - [setYAxisFontWeight](#setyaxisfontweight)
+    - [setXAxisFontColor](#setxaxisfontcolor)
+    - [setYAxisFontColor](#setyaxisfontcolor)
+    - [setXAxisSplitLine](#setxaxissplitline)
+    - [setYAxisSplitLine](#setyaxissplitline)
+    - [setXAxisInterval](#setxaxisinterval)
+    - [setYAxisInterval](#setyaxisinterval)
+    - [setAxisType](#setaxistype)
+
+<!-- END doctoc generated TOC please keep comment here to allow auto update -->
+
 
 
 ## api
+
+#### setTitle
 ***
-```charts.axis(axis, data, type,config);```
+```charts.setTitle(title):设置标题```
+1. title: 标题名称
+***
+
+#### setXAxisFontStyle
+***
+```charts.setXAxisFontStyle(style): 设置X轴的坐标字体```
+1. style: 
+    + 'normal'
+    + 'italic'
+    + 'oblique'
+***
+
+
+#### setYAxisFontStyle
+***
+```charts.setYAxisFontStyle(style): 设置Y轴的坐标字体```
+1. style: 
+    + 'normal'
+    + 'italic'
+    + 'oblique'
+***
+
+#### setXAxisFontWeight
+***
+```charts.setXAxisFontWeight(weight): 设置X轴的坐标间隔线```
+1. weight: number字符串
+***
+
+#### setYAxisFontWeight
+***
+```charts.setYAxisFontWeight(weight): 设置Y轴的坐标间隔线```
+1. weight: number字符串
+***
+
+#### setXAxisFontColor
+***
+```charts.setXAxisFontColor(rgb): 设置X轴的坐标字体颜色```
+1. rgb: 
+    + rgb格式
+    + color单词
+    + 数字像素值
+***
+
+#### setYAxisFontColor
+***
+```charts.setYAxisFontColor(rgb): 设置Y轴的坐标字体颜色```
+1. rgb: 
+    + rgb格式
+    + color单词
+    + 数字像素值
+***
+
+#### setXAxisSplitLine
+***
+```charts.setXAxisSplitLine(show): 设置X轴的坐标间隔线```
+1. show: true/false
+***
+
+#### setYAxisSplitLine
+***
+```charts.setAxisSplitLine(show): 设置Y轴的坐标间隔线```
+1. show: true/false
+
++ 同时设置为true，可形成坐标网格
+***
+
+
+#### setXAxisInterval
++ 坐标轴刻度标签的显示间隔，在类目轴中有效。
+***
+```charts.setXAxisInterval(weight): 设置X轴的坐标显示间隔```
+1. weight: number字符串
+***
+
+#### setYAxisInterval
++ 坐标轴刻度标签的显示间隔，在类目轴中有效。
+***
+```charts.setXAxisInterval(weight): 设置X轴的坐标显示间隔```
+1. weight: number字符串
+***
+
+
+#### setAxisType
+***
+```charts.setAxisType(axis, type, format): 坐标轴类型设置```
 
 1. axis:坐标轴类型，传入xAxis或yAxis
 2. type(默认为category):
@@ -10,9 +117,9 @@
     + category: 类目轴，适用于离散的类目数据。为该类型时类目数据可自动从 series.data 或 dataset.source 中取，或者可通过 xAxis.data 设置类目数据。
     + time: 时间轴，适用于连续的时序数据，与数值轴相比时间轴带有时间的格式化，在刻度计算上也有所不同，例如会根据跨度的范围来决定使用月，星期，日还是小时范围的刻度。
     + log: 对数轴。适用于对数数据。
-3. format:  (HH:mm:ss)/(yyyy-mm-dd HH:mm:ss),可以设置两种日期格式
-4. config:配置项(可选)
+3. format:  type=time时，支持两种日期格式，(HH:mm:ss)/(yyyy-mm-dd HH:mm:ss)
 ***
+
 
 
 
